81b77863136c131264463d65015424fb983781a5
vishal-Tamhane <vishaltamhane367@gmail.com>
2024-09-05 07:58:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.body.classList.add('loaded');
+
+//     document.querySelectorAll('a').forEach(link => {
+//         link.addEventListener('click', (e) => {
+//             e.preventDefault();
+//             const targetUrl = e.currentTarget.href;
+//             document.body.classList.add('fade-out');
+
+//             setTimeout(() => {
+//                 window.location.href = targetUrl;
+//             }, 500); // Match this to your transition duration
+//         });
+//     });
+// });
